f27c27f91fc1683e5c8ecc0349340b8c6040114b
alexpiven <a@piven.info>
2025-04-12 14:05:08 +0300

    lession1

---
+++ lession 1/less1.py
@@ -0,0 +1,7 @@
+import time
+
+a= 0
+while a < 10:
+    a+=1
+    print(a)
+    time.sleep(1)
---
+++ lession 2/les2.py
@@ -0,0 +1,6 @@
+from lesson_2_data import courts
+
+
+names = [i['court_name'] for i in courts ]
+
+print(names)
